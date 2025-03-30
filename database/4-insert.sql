a33e5fbc787dc9b35ca2d333d22c38ec23cc42c3
itssabrinaa <sabrinacassone04@gmail.com>
2025-03-30 18:11:47 +0200

    [DataBase] Correzioni e insert

---
+++ database/4-insert.sql
@@ -0,0 +1,35 @@
+
+INSERT INTO film (titolo, regista, genere, anno, durata, descrizione, numero_copie, numero_copie_disponibili)
+VALUES 
+('Inception', 'Christopher Nolan', 'Fantascienza', 2010, 148, 'Un ladro esperto nei sogni entra nella mente delle persone.', 5, 5),
+('La La Land', 'Damien Chazelle', 'Musical', 2016, 128, 'Il film racconta la storia d amore tra un musicista jazz (Ryan Gosling) e un aspirante attrice (Emma Stone).', 5, 5),
+('Orgoglio e pregiudizio', 'Joe Wright', 'Storico', 2005, 127, 'Il film basato sul celebre romanzo di Jane Austen.', 5, 5),
+('Ocean s 8', 'Gary Ross', 'Azione', 2018, 111, 'La sorella di Danny Ocean progetta un bel colpo per intascarsi un sacco di soldi. Ma per farlo ha bisogno di qualche complice.', 5, 5),
+('Barbie', 'Greta Gerwig', 'Commedia', 2023, 114, 'Il primo adattamento cinematografico in live action dell omonima fashion doll della Mattel.', 5, 5),
+('Indiana Jones 5', 'James Mangold', 'Avventura', 2023, 142, 'Il temerario archeologo Indiana Jones lotta contro il tempo per recuperare un quadrante leggendario che può cambiare il corso della storia.', 5, 5),
+('Dunkirk', 'Christopher Nolan', 'Drammatico', 2017, 106, 'L esercito inglese, impegnato a liberare le truppe alleate dalla Francia occupata dai nazisti, resta intrappolato sulla spiaggia.', 5, 5),
+('Le Cronache di Narnia', 'Andrew Adamson', 'Fantasy', 2005, 150, 'Quattro fratelli scoprono una porta misteriosa che li porta in un universo incantato dove una strega ha lanciato un tremendo incantesimo.', 5, 5),
+('Assassinio sull Orient Express', 'Kenneth Branagh', 'Giallo', 2017, 109, 'Un lussuoso viaggio a bordo dell Orient Express si trasforma in una corsa contro il tempo quando viene commesso un omicidio.', 5, 5),
+('Unfriended', 'Levan Gabriadze', 'Horror', 2015, 83, 'Tramite un programma di messaggistica, una presenza sovrannaturale si impossessa dell identità online di una ragazza scomparsa.', 5, 5),
+('La ragazza del treno', 'Tate Taylor', 'Thriller', 2016, 112, 'Una donna distrutta dalla fine del proprio matrimonio comincia ad osservare ossessivamente una coppia, apparentemente perfetta, fino a quando assiste ad una scena che la lascia scioccata.', 5, 5),
+('Il buono, il brutto, il cattivo', 'Sergio Leone', 'Western', 1966, 175, 'Mentre infuria la Guerra di Secessione, tre uomini dall oscuro passato si battono per impossessarsi di un tesoro nascosto in un cimitero.', 5, 5),
+('Toy Story', 'John Lasseter', 'Animazione', 1995, 81, 'La vita di Woody, un cowboy giocattolo e pupazzo preferito del suo piccolo padrono, è minacciata dall arrivo di Buzz Lightyear, un nuovo robot colorato e pieno di luci.', 5, 5),
+('Amy', 'Asif Kapadia', 'Documentario', 2015, 128, 'Amy è un documentario del 2015 diretto da Asif Kapadia sulla vita della cantante Amy Winehouse, morta a soli 27 anni per abuso di alcol.', 5, 5),
+('Colpa delle stelle', 'Josh Boone', 'Romantico', 2014, 126, 'Due giovani si conoscono durante le riunioni in un gruppo di sostegno per malati di cancro e si innamorano l uno dell altra.', 5, 5);
+
+
+/* INSERT INTO utente (nome, cognome, email, password_hash, film_non_restituiti)
+VALUES 
+('Mario', 'Rossi', 'mario.rossi@email.com', 'hash_password_1', FALSE),
+('Luca', 'Bianchi', 'luca.bianchi@email.com', 'hash_password_2', FALSE),
+('Giulia', 'Verdi', 'giulia.verdi@email.com', 'hash_password_3', FALSE),
+('Anna', 'Neri', 'anna.neri@email.com', 'hash_password_4', FALSE),
+('Marco', 'Gialli', 'marco.gialli@email.com', 'hash_password_5', FALSE); --Questi utenti non vanno bene poiche' la password non e' hashata dal nostro server.
+
+INSERT INTO noleggio (utente_id, film_id, data_noleggio)
+VALUES 
+(1, 1, '2025-03-20 10:00:00'),
+(2, 2, '2025-03-21 15:30:00'),
+(3, 3, '2025-03-22 18:45:00'),
+(4, 4, '2025-03-23 09:15:00'),
+(5, 5, '2025-03-24 12:00:00'); */
